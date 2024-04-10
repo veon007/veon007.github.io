11628cb11eb0fe7a7ff317776c905e9754a360d3
veon007 <166635911+veon007@users.noreply.github.com>
2024-04-10 10:25:40 -0700

    Add files via upload

---
+++ MozillaDemo/scripts/main.js
@@ -0,0 +1,67 @@
+const myImage = document.querySelector("img");
+const thundah = document.getElementById("bird");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
+
+thundah.onclick = () => {
+  const mySrc2 = thundah.getAttribute("src");
+  if (mySrc2 === "images/thunderbird.png") {
+    thundah.setAttribute("src", "images/thunderbirdOld.png");
+  } else {
+    thundah.setAttribute("src", "images/thunderbird.png");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
+
+
+//Konami Code Secret Shenanigens
+document.addEventListener('keydown', keyHandler);
+var pattern = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+var current = 0;
+function keyHandler() {
+	console.log("Hooray");
+	//current++;
+	//console.log(current);
+	
+	// If the key isn't in the pattern, or isn't the current key in the pattern, reset
+	if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
+		current = 0;
+		return;
+	}
+
+	// Update how much of the pattern is complete
+	current++;
+
+	// If complete, alert and reset
+	if (pattern.length === current) {
+		current = 0;
+		window.alert('30 Lives For You!');
+	}
+	
+};
